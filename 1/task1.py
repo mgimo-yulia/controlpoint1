a7c4e15aeac1de99a2afb29253483a9092391279
mgimo-yulia <mgimo-yulia@netsso.com>
2021-01-13 12:16:04 +0300

    modified:   1/task1.py

--- 1/task1.py
+++ 1/task1.py
@@ -1,3 +1,19 @@
 # -*- coding: 1251 -*-
 #Дан массив из 10 чисел.Сколько элементов массива больше своих «соседей», т.е. предыдущего и последующего.
 #Первый и последний элементы не рассматривать.
+
+def calculate_number_of_elements_greater_neighbors(a):
+  n=len(a);
+  i=1;res=0;
+  while i < n-1:
+    if a[i] > a[i-1]:
+     if a[i] > a[i+1]:
+      res=res+1;
+    i=i+i
+
+  return res;
+
+arr=[1,2,3,4,6,5,7,8,9,10]
+
+print ("Массив:",arr);
+print ("Количество элементов массива больше своих соседей:",calculate_number_of_elements_greater_neighbors(arr));
